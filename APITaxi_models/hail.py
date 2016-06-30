1c104c61d3d773940e8b1766aa1579c4e0566870
l-vincent-l <lara.vincent@gmail.com>
2016-06-30 11:18:56 +0200

    Add user haillog

--- APITaxi_models/hail.py
+++ APITaxi_models/hail.py
@@ -306,6 +306,7 @@ class HailLog(object):
                     "method": self.method,
                     "payload": self.payload,
                     "initial_status": self.initial_status,
+                    "user": current_user.email
         }
         if error:
             to_store['error'] = error
