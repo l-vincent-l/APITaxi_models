2b52347f9c354db0a877ca8d1434443f8fe94a27
l-vincent-l <lara.vincent@gmail.com>
2017-01-31 18:45:23 +0100

    Fix setup.py

--- APITaxi_models/__init__.py
+++ APITaxi_models/__init__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 1, 0)
-__author__ = 'Vincent Lara'
-__contact__ = "vincent.lara@data.gouv.fr"
-__homepage__ = "https://github.com/"
-__version__ = ".".join(map(str, VERSION))
-__doc__ = "Models used by APITaxi"
 try:
     from flask_sqlalchemy import SQLAlchemy
 except ImportError:
